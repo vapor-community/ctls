f79417c7edde35bd3e96b754e8ffe7a0353168c7
Logan Wright <logan.william.wright@gmail.com>
2017-08-04 15:51:52 +0200

    update manifests

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "CTLS",
     pkgConfig: "ctls",
     providers: [
-        .Brew("ctls"),
-        .Apt("ctls")
+        .brew(["ctls"]),
+        .apt(["ctls"])
     ]
 )
